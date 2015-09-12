9f8d8c6b8697aaec5b8961a634731b3af0a899d1
Sam <samarthkanabar@gmail.com>
2015-09-12 23:20:56 +0530

    AddExpenseActivity is where you'll have to add code to insert into the database.

---
+++ app/src/main/java/com/coditsuisse/team60/expensetracker/DatePickerFragment.java
@@ -0,0 +1,35 @@
+package com.coditsuisse.team60.expensetracker;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+/**
+ * Created by Samarth on 12-09-2015.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        month++;
+        StringBuilder expenseDate = (new StringBuilder().append(day).append("/").append(month).append("/").append(year));
+        EditText dateField = (EditText) getActivity().findViewById(R.id.expense_date);
+        dateField.setText(expenseDate);
+    }
+}
