a48a023121b6232135eea2e04b097786d32e5060
Kshitij Nagvekar <ksnagvekar@gmail.com>
2015-09-13 00:40:29 +0530

    MainActivity2

--- app/src/main/java/com/coditsuisse/team60/expensetracker/MainActivity2.java
+++ app/src/main/java/com/coditsuisse/team60/expensetracker/MainActivity2.java
@@ -1,24 +1,43 @@
 package com.coditsuisse.team60.expensetracker;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        Button setExpensesButton = (Button) findViewById(R.id.buttonAddExpenses);
+        setExpensesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), AddExpenseActivity.class));
+                finish();
+            }
+        });
+
+        Button setIncomeButton = (Button) findViewById(R.id.buttonAddIncome);
+        setIncomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        setContentView(R.layout.activity_main);
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_activity2, menu);
         return true;
     }
 
@@ -31,11 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
-        }
-
-        if (id == R.id.action_add_expense) {
-            startActivity(new Intent(this, MainActivity2.class));
             return true;
         }
 
